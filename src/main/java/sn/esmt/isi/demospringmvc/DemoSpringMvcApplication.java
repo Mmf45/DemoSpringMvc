3fa920db3a4598d4265fa336fd1f1992be4adc58
Mmf <fallmoustapha597@gmail.com>
2025-11-23 16:07:08 +0000

    first init

---
+++ src/main/java/sn/esmt/isi/demospringmvc/DemoSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package sn.esmt.isi.demospringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringMvcApplication.class, args);
+    }
+
+}
